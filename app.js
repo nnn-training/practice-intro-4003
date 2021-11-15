c200a2936630685bb12acd5dde7479d72edb38a4
Keigo Murata <kei555_go@ybb.ne.jp>
2021-11-15 20:17:58 +0900

    /users のページもGitHub認証がないと閲覧不可に変更

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.get('/logout', function (req, res) {
 });
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next() };
+  if (req.isAuthenticated()) { return next(); }
   res.redirect('/login');
 }
 
