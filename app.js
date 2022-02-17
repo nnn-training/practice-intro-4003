36f412ca27dfece4338f8d96170272ee7c423587
hana fleur <yukay2184@gmail.com>
2022-02-17 21:54:20 +0900

    /usersのページもGithub認証がなければ閲覧できないよう修正

--- app.js
+++ app.js
@@ -53,7 +53,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//第二引数にensureAuthenticated関数を設定することで、/usersのアクセスに認証が必要となる
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
@@ -75,6 +76,15 @@ app.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
+
+// ensureAuthenticated関数。認証が完了済みならnex関数を返し、そうでなければlogin画面にリダイレクト
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated())
+  {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
