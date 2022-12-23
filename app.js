68e30c493cbc65ec35365e2ba687410129fba8dc
nanayuuki2022 <nanayuuki2022@gmail.com>
2022-12-23 17:12:00 +0900

    C:/Program Files/Git/usersのページを認証が完了していないと/loginにリダイレクトさせるよう変更

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ passport.use(new GitHubStrategy({
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const photosRouter = require('./routes/photos');
+const { runInNewContext } = require('vm');
 
 var app = express();
 app.use(helmet());
@@ -53,7 +54,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
@@ -93,5 +94,12 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 module.exports = app;
