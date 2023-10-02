810c9b19edfdfecdd6a50d91669e94e81a2fea1e
kusupon <kusupon0628@gmail.com>
2023-10-02 15:15:39 +0900

    認証前に/usersへアクセスすると/loginにリダイレクトするように修正

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
@@ -80,6 +80,13 @@ app.get('/logout', (req, res, next) => {
   });
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -95,5 +102,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
