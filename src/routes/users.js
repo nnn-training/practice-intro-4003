fd01b5a287003a48b090101354ec5de4225f1c96
rin14len <251zs02465@student.zen.ac.jp>
2026-07-09 13:06:16 +0900

    C:/Program Files/Git/usersはログインなしでは見られないようにする

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,13 @@
 const { Hono } = require("hono");
+const { ensureAuthenticated } = require('../middlewares');
 
 const app = new Hono();
+
+app.use(ensureAuthenticated());
 
 app.get("/", (c) => {
   return c.text("respond with a resource");
 });
+
 
 module.exports = app;
